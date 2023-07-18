8f61c2e13247d3efa0e866281185aaec7071d6d2
xnki7 <codeinja7@gmail.com>
2023-07-19 01:49:48 +0530

    Added suggestion box and styled it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar"
 import Homepage from './Pages/Homepage/Homepage';
+import SuggestionBox from './Components/SuggestionBox/SuggestionBox';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Homepage/>
+      <SuggestionBox/>
     </div>
   );
 }
---
+++ src/Components/SuggestionBox/SuggestionBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./SuggestionBox.css"
+
+function SuggestionBox() {
+  return (
+    <div className='SuggestionBox'>
+        <div className="header">
+            <p className="suggestions">Suggestions</p>
+        </div>
+    </div>
+  )
+}
+
+export default SuggestionBox
