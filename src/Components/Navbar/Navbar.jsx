ecd7c9fdf17a00454a0b17aaffee5c50a3815ce3
xnki7 <codeinja7@gmail.com>
2023-07-18 21:41:27 +0530

    Basic Navbar styling done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,17 @@ import "./Navbar.css"
 
 function Navbar() {
   return (
-    <div className='Navbar'>Navbar</div>
+    <div className='Navbar'>
+      <div className="top">
+        <div className="logo"></div>
+        <p className="navBtn">Home</p>
+        <p className="navBtn">Bookmark</p>
+        <p className="navBtn">Profile</p>
+      </div>
+      <div className="bottom">
+        <button>Connect Wallet</button>
+      </div>
+    </div>
   )
 }
 
