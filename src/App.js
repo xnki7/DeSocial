facdeaec7f93e0a335c678eeebb986db5ffda8e8
xnki7 <codeinja7@gmail.com>
2023-08-04 00:22:00 +0530

    Connect Wallet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Navbar from "./Components/Navbar/Navbar"
 import Homepage from './Pages/Homepage/Homepage';
 import SuggestionBox from './Components/SuggestionBox/SuggestionBox';
 import Bookmarks from './Pages/Bookmarks/Bookmarks';
 
 function App() {
+  const [connected, setConnected] = useState(false);
+  const [account, setAccount] = useState(null);
   return (
     <div className="App">
-      <Navbar />
-      <Homepage/>
-      <SuggestionBox/>
+      <Navbar setAccount={setAccount} setConnected={setConnected} />
+      <Homepage />
+      <SuggestionBox />
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 import "./Navbar.css"
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
-function Navbar() {
+const { chains, publicClient } = configureChains(
+  [polygonMumbai],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "DeSocial",
+  projectId: "641b78ab05eb8ddcdc68fc15be6cd561",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+function Navbar({ setConnected, setAccount }) {
   return (
     <div className='Navbar'>
       <div className="top">
@@ -11,7 +34,72 @@ function Navbar() {
         <p className="navBtn">Profile</p>
       </div>
       <div className="bottom">
-        <button>Connect Wallet</button>
+        {/* <button>Connect Wallet</button> */}
+        <WagmiConfig config={wagmiConfig}>
+          <RainbowKitProvider chains={chains} modalSize="compact">
+            <ConnectButton.Custom>
+              {({
+                account,
+                chain,
+                openAccountModal,
+                openChainModal,
+                openConnectModal,
+                mounted,
+              }) => {
+                const ready = mounted;
+                const connected = ready && account && chain;
+                return (
+                  <div
+                    {...(!ready && {
+                      "aria-hidden": true,
+                      style: {
+                        opacity: 0,
+                        pointerEvents: "none",
+                        userSelect: "none",
+                      },
+                    })}
+                  >
+                    {(() => {
+                      if (!connected) {
+                        setAccount("");
+                        setConnected(false);
+
+                        return (
+                          <button onClick={openConnectModal} type="button">
+                            Connect Wallet
+                          </button>
+                        );
+                      }
+
+                      if (chain.unsupported) {
+                        return (
+                          <button onClick={openChainModal} type="button">
+                            Wrong network
+                          </button>
+                        );
+                      }
+                      setAccount(account.address);
+                      setConnected(true);
+
+                      return (
+
+                        <button
+                          onClick={() => {
+                            openAccountModal();
+                          }}
+                          type="button"
+                        >
+                          {account.displayName}
+                        </button>
+
+                      );
+                    })()}
+                  </div>
+                );
+              }}
+            </ConnectButton.Custom>
+          </RainbowKitProvider>
+        </WagmiConfig>
       </div>
     </div>
   )
